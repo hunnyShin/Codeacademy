ba41053acc6b8a317ebd6d8b0155f679f623335e
Hyunhee Shin <hee@heeui-MacBook-Air.local>
2014-02-03 01:08:11 -0600

    Animate my name
    Created by Lalith Polepeddi
    Support at Codecademy

---
+++ Animatemyname.js
@@ -0,0 +1,18 @@
+Animate my name
+
+var red = [0, 100, 63];
+var orange = [40, 100, 65];
+var green = [75, 100, 40];
+var blue = [196, 77 ,55];
+var purple = [280, 100, 30];
+
+var myName = "julieShin";
+var letter Colors = [red, green, blue, orange, purple];
+if (10 < 3) {
+	bubbleShape = "square";
+} else {
+	bubbleShape = "circle";
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
